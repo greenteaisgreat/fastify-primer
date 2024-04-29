d58e48e1b42ed8930750c9aa66b2a769cd3a569a
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-04-29 17:55:59 -0500

    Add Fastify Input to Route to greetingsController

--- src/index.js
+++ src/index.js
@@ -1,40 +1,22 @@
 import Fastify from "fastify";
+import greetingsController from "./middleware/greetingsController.js";
 
 const fastify = Fastify({
   logger: true,
 });
 
-const options = {
-  schema: {
-    query: {
-      properties: {
-        lastName: { type: "string" },
-      },
-      required: ["lastName"],
-    },
-    params: {
-      properties: {
-        //defines the structure of the 'name' parameter
-        name: { type: "string" },
-      },
-      //not that necessary, as if you specify in the URL scheme that
-      //a param is there (:name), it's required by default
-      required: ["name"],
-    },
-    //allows you to handle different response codes
-    response: {
-      200: {
-        properties: {
-          message: { type: "string" },
-        },
-        required: ["message"],
-      },
-    },
-  },
-};
+//fastify plugin that acts similarly to an express router
+fastify.register(greetingsController, { prefix: "/greetings" });
 
-//same exact functionality as fastify.get above, just a different
-//way to implement it; take note of the options object as well
+try {
+  fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(`There was a server error: ${err}`);
+  process.exit(1);
+}
+
+// //same exact functionality as fastify.get above, just a different
+// //way to implement it; take note of the options object as well
 // fastify.route({
 //   method: "GET",
 //   url: "/hello/:name",
@@ -73,10 +55,3 @@ const options = {
 //     };
 //   },
 // });
-
-try {
-  fastify.listen({ port: 3000 });
-} catch (err) {
-  fastify.log.error(`There was a server error: ${err}`);
-  process.exit(1);
-}
