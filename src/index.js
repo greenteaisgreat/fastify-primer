757139d6ad121c1b8820233456df27efb9ff3495
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-04-29 17:18:42 -0500

    Move GET Request Into its Own Middleware File

--- src/index.js
+++ src/index.js
@@ -33,50 +33,46 @@ const options = {
   },
 };
 
-fastify.get("/yo/:name", options, (req, res) => {
-  res.send({ message: `Hello, ${req.params.name} ${req.query.lastName}!` });
-});
-
 //same exact functionality as fastify.get above, just a different
 //way to implement it; take note of the options object as well
-fastify.route({
-  method: "GET",
-  url: "/hello/:name",
-  //defines a JSON schema
-  schema: {
-    query: {
-      properties: {
-        lastName: { type: "string" },
-      },
-      required: ["lastName"],
-    },
-    params: {
-      properties: {
-        //defines the structure of the 'name' parameter
-        name: { type: "string" },
-      },
-      //not that necessary, as if you specify in the URL scheme that
-      //a param is there (:name), it's required by default
-      required: ["name"],
-    },
-    //allows you to handle different response codes
-    response: {
-      200: {
-        properties: {
-          message: { type: "string" },
-        },
-        required: ["message"],
-      },
-    },
-  },
-  handler: (req, res) => {
-    return {
-      //will give an error if the key doesn't match the response schema
-      // message2: `Hello there, ${req.params.name}!`
-      message: `Hello there, ${req.params.name} ${req.query.lastName}!`,
-    };
-  },
-});
+// fastify.route({
+//   method: "GET",
+//   url: "/hello/:name",
+//   //defines a JSON schema
+//   schema: {
+//     query: {
+//       properties: {
+//         lastName: { type: "string" },
+//       },
+//       required: ["lastName"],
+//     },
+//     params: {
+//       properties: {
+//         //defines the structure of the 'name' parameter
+//         name: { type: "string" },
+//       },
+//       //not that necessary, as if you specify in the URL scheme that
+//       //a param is there (:name), it's required by default
+//       required: ["name"],
+//     },
+//     //allows you to handle different response codes
+//     response: {
+//       200: {
+//         properties: {
+//           message: { type: "string" },
+//         },
+//         required: ["message"],
+//       },
+//     },
+//   },
+//   handler: (req, res) => {
+//     return {
+//       //will give an error if the key doesn't match the response schema
+//       // message2: `Hello there, ${req.params.name}!`
+//       message: `Hello there, ${req.params.name} ${req.query.lastName}!`,
+//     };
+//   },
+// });
 
 try {
   fastify.listen({ port: 3000 });
