2550db2781d883ab0d880f67bc2d83c3ac916b82
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-04-29 20:33:16 -0500

    Add Response Schema & Query Params for GET Request

--- src/middleware/greetingsController.js
+++ src/middleware/greetingsController.js
@@ -1,13 +1,31 @@
 //fastify uses plugins to act as endpoint directives, rather than express
 //applications, which make use of routers to direct to a given endpoint
 
+//specifying a response schema improves performance within fastify,
+//as fastify serializes JSON formats very efficiently. this is REQUIRED
+//in production settings for people that use your application!
+const responseSchema = {
+  response: {
+    200: {
+      properties: {
+        message: { type: "string" },
+      },
+      required: ["message"],
+    },
+  },
+};
+
 //three params to indicate the:
 //1.) fastify application server - fastify
 //2.) optional headers - options
 //3.) the termination of the controller once it's finished - done
 const greetingsController = (fastify, options, done) => {
-  fastify.get("/", (req, res) => {
-    res.send({ message: `Hello, world!` });
+  fastify.get("/", { schema: responseSchema }, (req, res) => {
+    return { message: `Hello, world!` };
+  });
+
+  fastify.get("/:name", { schema: responseSchema }, (req, res) => {
+    return { message: `Hello, ${req.params.name}!` };
   });
 
   //must invoke done() to signal the end of the controller
@@ -16,12 +34,12 @@ const greetingsController = (fastify, options, done) => {
 
 // const options = {
 //   schema: {
-//     query: {
-//       properties: {
-//         lastName: { type: "string" },
-//       },
-//       required: ["lastName"],
-//     },
+//     // query: {
+//     //   properties: {
+//     //     lastName: { type: "string" },
+//     //   },
+//     //   required: ["lastName"],
+//     // },
 //     params: {
 //       properties: {
 //         //defines the structure of the 'name' parameter
